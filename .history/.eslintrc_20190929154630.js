3b89bb4ffa23747386a2dfae88537c31ac52fb60
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-09-29 17:05:49 +0900

    update

--- .history/.eslintrc_20190929154630.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "extends": [
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
-  ],
-  rules: {
-    '@typescript-eslint/consistent-type-assertions': 0,
-  }
-}
